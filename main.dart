932bd329c188eec41de9869bd3de894a871f36e5
ferhadquluzade <62217940+ferhadquluzade@users.noreply.github.com>
2021-02-15 20:52:24 +0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:log_in_ui/components/button.dart';
-import 'package:log_in_ui/components/check_box.dart';
-import 'package:log_in_ui/components/id_field.dart';
-import 'package:log_in_ui/components/password_field.dart';
+import 'package:logIn/button.dart';
+import 'package:logIn/check_box.dart';
+import 'package:logIn/password_field.dart';
+import 'package:logIn/button.dart';
+import 'package:logIn/check_box.dart';
+import 'package:logIn/id_field.dart';
+import 'package:logIn/password_field.dart';
+
+import 'id_field.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,43 +20,43 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
-        primarySwatch: Colors.blue,
-                     ),
+        primarySwatch: Colors.pink,
+      ),
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
- 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
   @override
   Widget build(BuildContext context) {
-        var size=MediaQuery.of(context).size; 
+    var size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body:   SingleChildScrollView(
-        child: Container(
-          
-            width:size.width,
-            height:size.height,
-          color: Color(0xFF4A148C),
+        body: SingleChildScrollView(
+      child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              gradient: LinearGradient(colors: [
+                Colors.pinkAccent,
+                Color(0xFF6F35A6),
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          width: size.width,
+          height: size.height,
           child: Column(
             children: [
-              SizedBox(height:size.height*0.35),
+              SizedBox(height: size.height * 0.29),
               IdTextField("ID"),
               PassTextField("Password"),
               Button(),
               CheckBox(),
             ],
           )),
-      )
-    );
+    ));
   }
 }
